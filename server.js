a4b7202840a9d3ccfd931e1d51bff6096aaa4b15
Antoni Pilarski <106629772+2DiabloPL@users.noreply.github.com>
2026-04-22 14:52:16 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require("mysql2/promise");
 const cors = require("cors");
 
-require("dotenv").config(); // lokalnie (na hostingu ENV i tak działa)
+
 
 const app = express();
 app.use(cors());
